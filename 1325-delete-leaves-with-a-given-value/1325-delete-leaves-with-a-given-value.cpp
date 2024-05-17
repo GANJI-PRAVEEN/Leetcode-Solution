3ad6f1f62a7afe34a1204bf0866f3c28ae9e63c7
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2024-05-17 20:01:11 +0530

    Time: 14 ms (30.93%), Space: 21.1 MB (98.58%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeaf(TreeNode* root){
+        return root->left==NULL and root->right==NULL;
+    }
+    TreeNode* solve(TreeNode* root,int k){
+        if(!root)return root;
+        root->left=solve(root->left,k);
+        root->right=solve(root->right,k);
+        if(isLeaf(root) and root->val==k){
+            return NULL;
+        }
+        return root;
+    }
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return solve(root,target);
+    }
+};
