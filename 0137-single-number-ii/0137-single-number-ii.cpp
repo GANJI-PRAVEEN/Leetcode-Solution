6f46a1b8e4b642eefed75a622af44c942ba958bd
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2024-05-19 09:53:59 +0530

    Time: 0 ms (100.00%), Space: 12 MB (29.57%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        int i=1;
         int n=nums.size();
-        int i=0;
-        while(i<n-1){
-            if(i+1<n and nums[i]!=nums[i+1]){
-                return nums[i];
-            }
-            else{
-                i+=2;
-            }
-            i++;
-            if(i>=n)break;
+        while(i<n){
+            if(nums[i-1]!=nums[i])return nums[i-1];
+            if(nums[i+1]!=nums[i])return nums[i+1];
+            i+=3;
         }
-        return nums[i];
+        return nums[n-1];
     }
 };
