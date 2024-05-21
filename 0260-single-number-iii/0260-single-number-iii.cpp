ceea284554367e550a78ae5946a988f2c1dd78ac
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2024-05-21 21:34:38 +0530

    Time: 10 ms (39.38%), Space: 14.3 MB (10.46%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i=0;
-        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
         vector<int>ans;
-        while(i<n){
-            if(i+1<n and nums[i]==nums[i+1]){
-                i+=2;
-            }
-            else {
-                ans.push_back(nums[i]);
-                i++;
+        for(auto it:mp){
+            if(it.second==1){
+                ans.push_back(it.first);
             }
         }
         return ans;
