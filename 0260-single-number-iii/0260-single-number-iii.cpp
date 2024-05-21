9e6d0ce82641009611a9dba48f2287dd6110efde
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2024-05-21 21:15:31 +0530

    Time: 3 ms (96.46%), Space: 12.6 MB (38.81%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    //Concept of Bucket one and Zero Bucket
     vector<int> singleNumber(vector<int>& nums) {
-        long xorr=0;
-        for(auto it:nums){
-            xorr^=it;
-        }
-        //Finding the rightMostBit of All elements XOR Value
-        long rightMostBit=(xorr&xorr-1)^xorr;
-        long oneBucket=0;
-        long zeroBucket=0;
-        for(auto it:nums){
-            if(it&rightMostBit){
-                oneBucket^=it;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int n=nums.size();
+        vector<int>ans;
+        while(i<n){
+            if(i+1<n and nums[i]==nums[i+1]){
+                i+=2;
             }
-            else{
-                zeroBucket^=it;
+            else {
+                ans.push_back(nums[i]);
+                i++;
             }
         }
-        return {(int)oneBucket,(int)zeroBucket};
+        return ans;
     }
 };
