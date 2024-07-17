2fd5e8f1d061f7a1767354ae82a8d7b14f0a4040
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2024-07-17 19:24:58 +0530

    Time: 3 ms (29.61%), Space: 7.7 MB (6.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -11,15 +11,14 @@ public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
         dp[0]=1;
+        long long first=1;
+        long long second=1;
         for(int i=1;i<=n;i++){
-            int l=dp[i-1];
-            int r=0;
-            if(i-2>=0){
-                r=dp[i-2];
-            }
-            dp[i]=l+r;
+            long long temp=first+second;
+            first=second;
+            second=temp;
         }
-        return dp[n];
+        return first;
         
     }
 };
