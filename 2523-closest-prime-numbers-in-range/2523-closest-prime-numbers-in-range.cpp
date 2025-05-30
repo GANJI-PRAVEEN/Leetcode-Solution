664cda06a3eec001866c34595f089a890f28cfcd
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-05-30 10:10:07 +0530

    Time: 375 ms (23.51%), Space: 200.5 MB (7.67%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.cpp
@@ -11,11 +11,25 @@ public:
         }
         return cnt==2;
     }
+    vector<int>precompute(int n){
+        vector<int>primes(n+1,1);
+        primes[0]=0;
+        primes[1]=0;
+        for(int i=2;i<=n;i++){
+            if(primes[i]==1){
+                for(int j=2*i;j<=n;j+=i){
+                    primes[j]=0;
+                }
+            }
+        }
+        return primes;
+    }
     vector<int> closestPrimes(int left, int right) {
         vector<int>ans;
         map<int,vector<int>>mp;
+        vector<int>primes = precompute(right);
         for(int i=left;i<=right;i++){
-            if(prime(i)){
+            if(primes[i]){
                 ans.push_back(i);
             }
         }
@@ -36,7 +50,6 @@ public:
                 }
             }
         }
-        if(n<=1)return {-1,-1};
         return arr;
     }
 };
