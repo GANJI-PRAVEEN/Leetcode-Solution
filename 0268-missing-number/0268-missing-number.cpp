114f2f36c17e07ff3ae7c552c570824ab2d622a4
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-06-10 10:58:47 +0530

    Time: 0 ms (100%), Space: 22.5 MB (5.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public: 
     int bruteForce(vector<int>arr,int n){
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            if(arr[i]!=i)return i;
+        }
+        if(arr[n-1]!=n)return n;
+        return -1;
+
+    }
+    int betterApproach(vector<int>arr,int n){
         vector<bool>mark(n+1,false);
         for(auto it:arr){
             mark[it]=true;
@@ -10,18 +19,18 @@ public:
         }
         return -1;
     }
-    int betterApproach(vector<int>arr,int n){
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
-            if(arr[i]!=i)return i;
+    int optimalApproach(vector<int>arr,int n){
+        int sum = 0;
+        for(auto it:arr){
+            sum+=it;
         }
-        if(arr[n-1]!=n)return n;
-        return -1;
-
+        int s2 = (n*(n+1))/2;
+        return s2-sum;
     }
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
         // return bruteForce(nums,n);
-        return betterApproach(nums,n);
+        // return betterApproach(nums,n);
+        return optimalApproach(nums,n);
     }
 };
