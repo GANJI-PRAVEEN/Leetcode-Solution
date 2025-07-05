1c494595f11d68745420a719dd6237531f49fbbf
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-07-05 18:52:41 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,46 +1,15 @@
 class Solution {
-public: 
-    int bruteForce(vector<int>arr,int n){
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
-            if(arr[i]!=i)return i;
-        }
-        if(arr[n-1]!=n)return n;
-        return -1;
-
-    }
-    int betterApproach(vector<int>arr,int n){
-        vector<bool>mark(n+1,false);
-        for(auto it:arr){
-            mark[it]=true;
-        }
-        for(int i=0;i<=n;i++){
-            if(!mark[i])return i;
-        }
-        return -1;
-    }
-    int optimalApproach(vector<int>arr,int n){
-        int sum = 0;
-        for(auto it:arr){
-            sum+=it;
-        }
-        int s2 = (n*(n+1))/2;
-        return s2-sum;
-    }
-    int mostOptimised(vector<int>arr,int n){
-        int xor1 = 0;
-        int xor2 =0;
-        for(int i=0;i<n;i++){
-            xor1^=(i+1);
-            xor2^=arr[i];
-        }
-        return xor1^xor2;
-    }
+public:
     int missingNumber(vector<int>& nums) {
+        int xor1=0;
+        int xor2=0;
         int n=nums.size();
-        // return bruteForce(nums,n);
-        // return betterApproach(nums,n);
-        // return optimalApproach(nums,n);
-        return mostOptimised(nums,n);
+        for(int i=0;i<=n;i++){
+            xor1^=i;
+        } 
+        for(auto it:nums)   {
+            xor2^=it;
+        }  
+        return xor1^xor2;      
     }
 };
