8f12fa23382388fe4124de765b1e7bcf361b51cc
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-07-23 14:32:55 +0530

    Time: 15 ms (43.84%), Space: 50.4 MB (30.32%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int>mp;
+    vector<int>bruteForce(vector<int>nums){
+         unordered_map<int,int>mp;
         for(auto it:nums){
             mp[it]++;
         }
@@ -10,5 +10,20 @@ public:
             if(it.second>1)ans.push_back(it.first);
         }
         return ans;
+    }
+    vector<int>betterApproach(vector<int>nums){
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+    vector<int> findDuplicates(vector<int>& nums) {
+        // return bruteForce(nums);
+        return betterApproach(nums);
     }
 };
