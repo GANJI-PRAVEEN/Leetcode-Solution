31c4601da91309f8388709f5a38314d63f5b00a5
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-07-23 14:21:53 +0530

    Time: 63 ms (24.04%), Space: 93.1 MB (10.38%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        for(auto it:mp){
+            if(it.second>1)return true;
+        }
+        return false;
+    }
+};
