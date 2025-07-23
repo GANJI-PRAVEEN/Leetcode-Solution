525a2f7c36a0e3eca036e05cf10ed80784c68b57
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-07-23 14:28:28 +0530

    Time: 61 ms (13.13%), Space: 67.2 MB (8.76%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        vector<int>ans;
+        for(auto it:mp){
+            if(it.second>1)ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
