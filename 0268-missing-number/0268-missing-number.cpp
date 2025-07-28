6104024bf4e062c1e7637e22539a3a40d00df4c7
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-07-28 23:25:06 +0530

    Time: 650 ms (5.09%), Space: 356.2 MB (5.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int bruteForce(vector<int>nums){
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i)return i;
-        }
-        return n;
-    }
-    int betterApproach(vector<int>nums){
-        int n=nums.size();
-        int sum1 = (n*(n+1))/2;
-        int sum2=0;
+    bool isExist(vector<int>nums,int i){
         for(auto it:nums){
-            sum2+=it;
+            if(it==i){
+                return true;
+            }
         }
-        return sum1-sum2;
+        return false;
     }
     int missingNumber(vector<int>& nums) {
-        // return bruteForce(nums);
-        return betterApproach(nums);
+        int n=nums.size();
+        for(int i=0;i<=n;i++){
+            if(!isExist(nums,i)){
+                return i;
+            }
+        }
+        return -1;
     }
 };
