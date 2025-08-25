6e9148e0c5e4f19356ac7c3a078a61a9c708b942
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-08-25 23:19:54 +0530

    Time: 1797 ms (5%), Space: 551.1 MB (5%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        set<vector<int>>hashSet;
-        vector<vector<int>> ans;
+    vector<vector<int>> bruteForce(vector<int>& nums, int target,int n){
+        set<vector<int>>hash;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int k = j+1;
-                int h = n-1;
-                while(k<h){
-                    long  sum = (long)((long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[h]);
-                    if(sum==target){
-                        hashSet.insert({nums[i],nums[j],nums[k],nums[h]});
-                        k++;
-                        h--;
+                for(int k=j+1;k<n;k++){
+                    for(int l=k+1;l<n;l++){
+                        long  sum = (long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[l];
+                        vector<int>temp = {nums[i],nums[j],nums[k],nums[l]};
+                        if(sum==target){
+                            sort(temp.begin(),temp.end());
+                            hash.insert(temp);
+                        }
                     }
-                    else if(sum>target)h--;
-                    else k++;
                 }
             }
         }
-        for(auto it:hashSet){
-            ans.push_back(it);
-        }
+        vector<vector<int>>ans(hash.begin(),hash.end());
         return ans;
+
+    }
+    vector<vector<int>> betterApproach(vector<int>& nums, int target,int n) {
+        set<vector<int>>hash;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n;j++){
+                int k=j+1;
+                unordered_map<long,int>store;;
+                while(k<n){
+                    long fourth =(long)((long)target-(long)((long)nums[i]+(long)nums[j]+(long)nums[k]));
+                    // cout<<fourth<<endl;
+                    if(store.find(fourth)!=store.end()){
+                        vector<int>temp={nums[i],nums[j],nums[k],(int)fourth};
+                        sort(temp.begin(),temp.end());
+                        hash.insert(temp);
+                    }
+                    store[nums[k]]++;
+                    k++;
+                }
+            }
+        }
+        vector<vector<int>>ans(hash.begin(),hash.end());
+        return ans;
+    }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        // return bruteForce(nums,target,n);
+        return betterApproach(nums,target,n);
     }
 };
