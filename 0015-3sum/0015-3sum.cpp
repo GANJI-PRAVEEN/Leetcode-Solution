10b3083489aed95c20402e45020a9cfef1f51d41
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-08-31 20:11:36 +0530

    Time: 2893 ms (5.01%), Space: 510.3 MB (5.2%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>>s;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
+    vector<vector<int>>bruteForce(vector<int>nums,int n){
+        set<vector<int>>st;
         for(int i=0;i<n;i++){
-            int l=i+1;
-            int r=n-1;
-            while(l<r){
-                int sum=nums[i]+nums[l]+nums[r];
-                if(sum==0){
-                    s.insert({nums[i],nums[l],nums[r]});
-                    l++;
-                    r--;
-                }
-                else if(sum>0){
-                     r--;   
-                }
-                else{
-                    l++;
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if(sum==0){
+                        vector<int>temp ={nums[i],nums[j],nums[k]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
                 }
             }
         }
-        vector<vector<int>>ans;
-        for(auto it:s){
-            ans.push_back(it);
-        }
+        vector<vector<int>>ans(st.begin(),st.end());
         return ans;
+    }
+    vector<vector<int>>betterApproach(vector<int>nums,int n){
+        unordered_map<int,int>mp;
+        set<vector<int>>st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int third = -(nums[i]+nums[j]);
+                if(mp.find(third)!=mp.end()){
+                    vector<int>temp = {nums[i],nums[j],third};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+            }
+            mp[nums[i]]++;
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+    }
+
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        // return bruteForce(nums,n);
+        return betterApproach(nums,n);
     }
 };
