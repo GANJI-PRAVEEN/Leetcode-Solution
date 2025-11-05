9e9f190d875cac37e4a4f0c1d5078452a64dbccb
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2025-11-05 23:24:34 +0530

    Time: 1245 ms (8.04%), Space: 291.9 MB (9.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>bruteForce(vector<int>nums,int n){
-        set<vector<int>>st;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if(sum==0){
-                        vector<int>temp ={nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                    }
-                }
-            }
-        }
-        vector<vector<int>>ans(st.begin(),st.end());
-        return ans;
-    }
-    vector<vector<int>>betterApproach(vector<int>nums,int n){
-        unordered_map<int,int>mp;
-        set<vector<int>>st;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int third = -(nums[i]+nums[j]);
-                if(mp.find(third)!=mp.end()){
-                    vector<int>temp = {nums[i],nums[j],third};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
-                }
-            }
-            mp[nums[i]]++;
-        }
-        vector<vector<int>>ans(st.begin(),st.end());
-        return ans;
-    }
-
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>>optimal(vector<int>nums){
         int n=nums.size();
-        // return bruteForce(nums,n);
-        return betterApproach(nums,n);
+        set<vector<int>>st;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    st.insert({nums[i],nums[j],nums[k]});
+                    j++;k--;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else j++;
+            }
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        return optimal(nums);
     }
 };
