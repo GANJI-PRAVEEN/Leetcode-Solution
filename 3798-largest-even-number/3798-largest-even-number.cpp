71192f494d378df87b8fb995a75e6fab95c785d0
GPraveen <132834383+GANJI-PRAVEEN@users.noreply.github.com>
2026-01-04 12:49:47 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.93%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestEven(string s) {
+        int i;
+        for(i=s.size()-1;i>=0;i--){
+            // cout<<s[i]-'0'<<endl;
+            if(s[i]=='2'){
+                break;
+            }
+        }
+        return s.substr(0,i+1);
+
+    }
+};
